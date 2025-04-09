357d1617897b6747cd35560e4bee9570567f5a09
Mahesh Lal <mahesh.2910@gmail.com>
2025-04-09 13:40:37 +0530

    Can now check if two lines are intersect

--- code_foundation_python/code_foundation/core/geometry/basics.py
+++ code_foundation_python/code_foundation/core/geometry/basics.py
@@ -5,10 +5,10 @@ class Line:
     def __init__(self, beginning: tuple, end: tuple):
         self.x1, self.y1 = beginning
         self.x2, self.y2 = end
-
+    
     def length(self):
         return ((self.x2 - self.x1) ** 2 + (self.y2 - self.y1) ** 2) ** 0.5
-
+    
     def slope(self):
         if self.x2 - self.x1 == 0:
             return float('inf')
@@ -18,6 +18,7 @@ class Line:
 def parallel_lines(line1: Line, line2: Line) -> bool:
     return math.isclose(line1.slope(), line2.slope())
 
+
 def perpendicular_lines(line1: Line, line2: Line) -> bool:
     if abs(line1.slope()) == float('inf') and line2.slope() == 0:  # line1 is vertical, line2 is horizontal
         return True
@@ -25,3 +26,7 @@ def perpendicular_lines(line1: Line, line2: Line) -> bool:
         return True
     if abs(line1.slope()) != float('inf') and abs(line2.slope()) != float('inf'):
         return math.isclose(line1.slope() * line2.slope(), -1)
+
+
+def intersects(line1: Line, line2: Line) -> bool:
+    return not (parallel_lines(line1, line2))
--- code_foundation_python/tests/test_core/geometry/test_basics.py
+++ code_foundation_python/tests/test_core/geometry/test_basics.py
@@ -1,48 +1,74 @@
 import unittest
 
-from code_foundation.core.geometry.basics import Line, parallel_lines, perpendicular_lines
+from code_foundation.core.geometry.basics import Line, parallel_lines, perpendicular_lines, intersects
 
 
 class TestLine(unittest.TestCase):
-
+    
     def test_line_length(self):
         line = Line((1, 2), (4, 6))
         length = line.length()
         self.assertAlmostEqual(length, 5.0)
-
+    
     def test_should_return_slope_of_line(self):
         line1 = Line((0, 0), (5, 5))
         self.assertEqual(line1.slope(), 1.0)
 
 
-class TestLineFunctions(unittest.TestCase):
-
+class TestParallelLines(unittest.TestCase):
+    
     def test_parallel_lines(self):
         line1 = Line((0, 0), (1, 1))
         line2 = Line((2, 2), (3, 3))
         self.assertTrue(parallel_lines(line1, line2))
-
-    def test_vertical_and_horizontal_lines_perpendicular(self):
-        line1 = Line((0, 0), (0, 5))  # Vertical line
-        line2 = Line((0, 0), (5, 0))  # Horizontal line
-        self.assertTrue(perpendicular_lines(line1, line2))
-
+    
     def test_not_parallel_lines(self):
         line1 = Line((0, 0), (1, 2))
         line2 = Line((0, 0), (2, 1))
         self.assertFalse(parallel_lines(line1, line2))
 
-    def test_perpendicular_lines(self):
+
+class TestPerpendicularLines(unittest.TestCase):
+    
+    def test_perpendicular_lines_which_are_not_vertical_or_horizontal(self):
         line1 = Line((1, 1), (3, 3))
         line2 = Line((2, 0), (0, 2))
         self.assertTrue(perpendicular_lines(line1, line2))
-
+    
     def test_non_perpendicular_lines(self):
         line1 = Line((0, 0), (2, 1))
         line2 = Line((1, 1), (3, 3))
         self.assertFalse(perpendicular_lines(line1, line2))
-
-    def test_horizontal_and_vertical_lines_are_perpendicular(self):
-        line1 = Line((0, 0), (5, 0))  # Horizontal line
-        line2 = Line((0, 0), (0, 5))  # Vertical line
+    
+    def test_vertical_and_horizontal_lines_perpendicular(self):
+        line1 = Line((0, 0), (0, 5))  # Vertical line
+        line2 = Line((0, 0), (5, 0))  # Horizontal line
         self.assertTrue(perpendicular_lines(line1, line2))
+    
+    def test_horizontal_and_vertical_lines_are_perpendicular(self):
+        line1 = Line((0, 0), (5, 0))
+        line2 = Line((0, 0), (0, 5))
+        self.assertTrue(perpendicular_lines(line1, line2))
+
+
+class TestLineIntersection(unittest.TestCase):
+    
+    def test_vertical_and_diagonal_lines_intersect(self):
+        line1 = Line((1, 0), (1, 5))
+        line2 = Line((0, 1), (2, 3))
+        self.assertTrue(intersects(line1, line2))
+    
+    def test_horizontal_and_diagonal_lines_intersect(self):
+        line1 = Line((0, 1), (0, 5))
+        line2 = Line((0, 1), (2, 3))
+        self.assertTrue(intersects(line1, line2))
+    
+    def test_perpendicular_lines_should_intersect(self):
+        line1 = Line((1, 1), (3, 3))
+        line2 = Line((2, 0), (0, 2))
+        self.assertTrue(intersects(line1, line2))
+    
+    def test_parallel_lines_should_not_intersect(self):
+        line1 = Line((0, 0), (1, 1))
+        line2 = Line((2, 2), (3, 3))
+        self.assertFalse(intersects(line1, line2))
